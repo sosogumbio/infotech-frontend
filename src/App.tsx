11f8fbaea3cd61c532ce9370b9d54f6bfe8664a2
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-09-04 11:22:42 -0300

    feat: fazendo o app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PHome from './pages/PHome/PHome'
 import PLogin from './pages/PLogin/PLogin'
-
+import PListaProdutos from './pages/PListagemProduto/PListagemProduto'
 
 function App() {
-
   return (
     <BrowserRouter>
-
       <Routes>
         <Route path='/' element={<PHome />} />
         <Route path='/login' element={<PLogin />} />
-        <Route
-        />
+        <Route path='/lista/produtos' element={<PListaProdutos />} />
       </Routes>
     </BrowserRouter>
   )
