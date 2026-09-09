78c5de82c12250e5d29da98d70b9905c307e36b5
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-09-09 11:30:03 -0300

    feat: Atualizando App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,67 @@
-
 import "./App.css";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 import PHome from "./pages/PHome/PHome";
 import PLogin from "./pages/PLogin/PLogin";
-
-
 import PListagemProduto from "./pages/PListagemProduto/PListagemProduto";
-import PDetalhesProduto from "./pages/PDetalhesProduto/PDetalhesProduto";
-
-
-
 import PListagemCategoria from "./pages/PListagemCategorias/PListagemCategorias";
-import PDetalhesCategoria from "./pages/PDetalhesCategoria/PDetalhesCategoria";
-import PAtualizarCategoria from "./pages/PAtualizar/PAtualizarCategoria/PAtualizarCategoria";
 import PListagemMovimentacao from "./pages/PListagemMovimentacoes/PListagemMovimentacoes";
+import PDetalhesCategoria from "./pages/PDetalhesCategoria/PDetalhesCategoria";
 import PDetalhesMovimentacao from "./pages/PDetalhesMovimentacao/PDetalhesMovimentacao";
-import PAtualizarMovimentacao from "./pages/PAtualizar/PAtualizarMovimentacao/PAtualizarMovimentacao";
+import PDetalhesProduto from "./pages/PDetalhesProduto/PDetalhesProduto";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
 
-
                 <Route
                     path="/"
                     element={<PHome />}
                 />
 
-               
                 <Route
                     path="/login"
                     element={<PLogin />}
                 />
 
-                
-
+     
                 <Route
                     path="/lista/produtos"
                     element={<PListagemProduto />}
                 />
 
-                <Route
-                    path="/detalhes/produto/:id"
-                    element={<PDetalhesProduto />}
-                />
-
-               
-
+          
                 <Route
                     path="/lista/categorias"
                     element={<PListagemCategoria />}
                 />
 
                 <Route
-                    path="/detalhes/categoria/:id"
-                    element={<PDetalhesCategoria />}
-                />
-
-                <Route
-                    path="/atualizar/categoria/:id"
-                    element={<PAtualizarCategoria />}
-                />
-
-             
-                <Route
                     path="/lista/movimentacoes"
                     element={<PListagemMovimentacao />}
                 />
+                
+                 <Route
+                    path="/detalhes/categoria/:id"
+                    element={<PDetalhesCategoria />}
+                />
+                
 
-                <Route
+                 <Route
                     path="/detalhes/movimentacao/:id"
                     element={<PDetalhesMovimentacao />}
                 />
-
-                <Route
-                    path="/atualizar/movimentacao/:id"
-                    element={<PAtualizarMovimentacao />}
+                
+                 <Route
+                    path="/detalhes/produto/:id"
+                    element={<PDetalhesProduto />}
                 />
-
+                
             </Routes>
         </BrowserRouter>
     );
